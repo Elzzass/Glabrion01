743daceadf3a19ce00be3391fa159e5858cf47ce
Elzzass <planetavega@yandex.ru>
2019-12-20 13:18:32 +0300

    Delete generalPresener inject from second fragment

--- app/src/main/java/name/bagi/levente/glabrion01/main/screen/newScreen/SecondFragment.kt
+++ app/src/main/java/name/bagi/levente/glabrion01/main/screen/newScreen/SecondFragment.kt
@@ -7,18 +7,14 @@ import android.view.ViewGroup
 import name.bagi.levente.glabrion01.R
 import name.bagi.levente.glabrion01.base.view.BaseFragment
 import name.bagi.levente.glabrion01.main.screen.general.GeneralContractInterface
-import org.koin.android.ext.android.inject
 
 class SecondFragment : BaseFragment(),
     GeneralContractInterface.View {
-    private val generalPresenter: GeneralContractInterface.Presenter by inject()
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-        generalPresenter.attach(this)
 
         val view = inflater.inflate(R.layout.fragment_second, container, false)
 
