8e03deecc27663a59943e488cd366752c751f680
Elzzass <planetavega@yandex.ru>
2019-12-22 15:23:58 +0300

    refactore extentions.kt

--- app/src/main/java/name/bagi/levente/glabrion01/common/extensions.kt
+++ app/src/main/java/name/bagi/levente/glabrion01/common/extensions.kt
@@ -2,7 +2,6 @@ package name.bagi.levente.glabrion01.common
 
 import android.view.View
 import android.widget.Toast
-import kotlinx.coroutines.CoroutineScope
 import name.bagi.levente.glabrion01.base.view.BaseFragment
 
 interface ProgressManager {
@@ -10,28 +9,10 @@ interface ProgressManager {
     fun hideProgress()
 }
 
-fun BaseFragment.hideProgress() {
-    (activity as? ProgressManager)?.hideProgress()
-}
-
-fun BaseFragment.showProgress() {
-    (activity as? ProgressManager)?.showProgress()
-}
-
 fun BaseFragment.toast(msg: String) {
     activity?.apply {
         Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
     }
-}
-
-fun <T, D> T.request(
-    showProgress: Boolean = true,
-    onComplete: (D) -> Unit
-) where T : BaseFragment, T : CoroutineScope {
-    if (showProgress) {
-        showProgress()
-    }
-
 }
 
 fun View.visible() {
